7dbacf6a5c83a6888cf301af7839c2362a2bd31b
Ksu <oks.leti@yandex.ru>
2023-06-20 21:17:09 +0300

    с проги

---
+++ 1.0.py
@@ -0,0 +1,37 @@
+# number = 2
+# c = str(number)
+# number2 = 5
+# d = str(number2)
+# sum = c+d
+# print(sum)
+#
+# stroka = '2'
+# a = int(stroka)
+# stroka2 = '5'
+# b = int(stroka2)
+# sum2 = a+b
+# print(sum2)
+
+# path 2
+
+# n = int(input())
+# m = int(input())
+# # sum = n/m
+#
+# print(n%m)
+
+# path 3
+
+name = input('Как звать? ')
+age = int(input('Скок лет то? '))
+
+if age<=30:
+    print(f'Алоха, {name}!')
+else:
+    print(f'Покеда, {name}!')
+
+print('тхе енд')
+
+# path 4
+
+
